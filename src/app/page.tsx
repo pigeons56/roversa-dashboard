2d4ed918dc6f6e3fe35dd00a8dd181e9e7429328
pigeons56 <lowpolypigeons@gmail.com>
2025-03-30 17:14:35 -0400

    Add welcome message, start form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.page}>Hello</div>;
+  return (
+    <div className={styles.page}>
+      <h1>Welcome to the Roversa Teacher Dashboard!</h1>
+      <div className={styles.login_box}>
+        <h2>Login</h2>
+      </div>
+    </div>
+  );
 }
