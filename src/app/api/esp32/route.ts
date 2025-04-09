8983aa2f76012c76d402ba7d172c80ebe6dc2ccd
pigeons56 <lowpolypigeons@gmail.com>
2025-04-09 16:56:55 -0400

    add console logs

--- src/app/api/esp32/route.ts
+++ src/app/api/esp32/route.ts
@@ -2,19 +2,23 @@ import { NextResponse } from "next/server";
 import { addRoversaOutput } from "@/app/db/update";
 
 export async function POST(request: Request) {
+  console.log("POST from esp32 received.");
   const data = await request.json();
   const roversaID = parseInt(data.roversaID);
   const program = data.program;
   const battery = parseFloat(data.battery);
 
+  console.log("Sending request to Mariadb...");
   const success = await addRoversaOutput(roversaID, program, battery);
 
   if (success == 1) {
+    console.log("Database successfully updated.");
     return NextResponse.json(
       { error: "Roversa output added." },
       { status: 200 }
     );
   } else {
+    console.log("Database update failed.");
     return NextResponse.json(
       { error: "Roversa output not added." },
       { status: 400 }
